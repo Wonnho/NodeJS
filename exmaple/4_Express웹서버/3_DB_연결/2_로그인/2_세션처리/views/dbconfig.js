a2e4f98db95a5f56d72204ddbf38142e29989d17
Choi Wonnho <wonnho@nate.com>
2024-02-15 18:35:48 +0900

    arrange folder tree

--- exmaple/4_Express웹서버/3_DB_연결/2_로그인/2_세션처리/views/dbconfig.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    user          :"open_source",
-    password      : "111",
-    connectString : "localhost:1521/xe",
-    // connectString : "localhost/xe",
-    externalAuth  : false
-};
